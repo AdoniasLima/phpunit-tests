7b4836b6d7f5fa8b2fefe63d70c70e0729af4bd4
Adonias <helpdesk1@safetydocs.com.br>
2020-06-18 10:15:53 -0300

    [DEV] - criando e realizando testes com dataProvider

--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -3,7 +3,8 @@
 use \PHPUnit\Framework\TestCase;
 
 class CalculadoraTest extends TestCase 
-{
+{	
+
 	public function testSoma()
 	{
 		$calculadora = new Calculadora();
@@ -20,5 +21,24 @@ class CalculadoraTest extends TestCase
 	{
 		$calculadora = new Calculadora();
 		$this->assertEquals( 60, $calculadora->soma(50, 9) );
+	}
+
+	/**
+	* @dataProvider somaDataProvider
+	*/
+	public function testeSomarDataProvider($numero1, $numero2, $esperado)
+	{
+		$calculadora = new Calculadora();
+		$this->assertEquals( $esperado, $calculadora->soma($numero1, $numero2) );
+	}
+
+	public function somaDataProvider()
+	{
+		return array(
+			array(1, 1, 2),
+			array(20, 10, 30),
+			array(-100, 30, -70),
+			array(10.5, 0.5, 11.0)
+		);
 	}
 }
